eac78b3c7cb2d3cc38b2727b80685fd7a9837e32
Thiagots05 <thiago-recife@hotmail.com>
2023-10-22 22:10:21 -0300

    [main] Criada consulta via http, exibição de últimas buscas e ajustes de layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +14,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -33,14 +35,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+    var cepController = TextEditingController();
+    List listaConsultas = [];
+    var rua = '';
+    var cidade = '';
+    var uf = '';
 
   
 
   @override
   Widget build(BuildContext context) {
-    var cepController = TextEditingController();
-    List listaConsultas = [1,2,3,4,5];
+
     return Scaffold(
       appBar: AppBar(
        
@@ -64,17 +69,21 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               TextField(
-                
+                maxLength: 8,
                 controller: cepController,
                 keyboardType: TextInputType.number,
                 
               ),
 
               const SizedBox(height: 20),
-              Text(
-                'Rua Direita \n Crato - CE',
-                style: Theme.of(context).textTheme.headlineMedium,
-                
+              Wrap(
+                children: [
+                  Text(
+                    '$rua \n $cidade - $uf',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                    
+                  ),
+                ],
               ),
               const Padding(
                               padding: EdgeInsets.fromLTRB(8,20,0,10),
@@ -95,14 +104,14 @@ class _MyHomePageState extends State<MyHomePage> {
                               
                               elevation: 2,
                               child: ListTile(
-                              title: const Row(
+                              title:  Row(
                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text('Rua Direita', style:  TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blueAccent),),
+                                  Text(e['rua'], style:  const TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.blueAccent),),
                
                                 ],
                               ),
-                              subtitle: const Text('Crato - CE', style: const TextStyle(color: Colors.grey),),
+                              subtitle: Text('${e['cidade']} - ${e['uf']}', style: const TextStyle(color: Colors.grey),),
                               onTap: () {
                                 Navigator.pop(context);
                               },
@@ -116,7 +125,25 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: ()async{
+          var result = await  http.get(Uri.parse("https://viacep.com.br/ws/${cepController.text}/json/"));
+          var resultMap = jsonDecode(result.body);
+          setState(() {
+          rua = resultMap['logradouro'].toString();
+          cidade = resultMap['localidade'].toString();
+          uf = resultMap['uf'].toString();
+          
+          
+          Map dadosCep = {
+            'cep':cepController.text,
+            'rua':rua,
+            'cidade': cidade,
+            'uf':uf,
+          };
+          listaConsultas.add(dadosCep);
+          });
+          print(listaConsultas);
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.search),
       ), 
